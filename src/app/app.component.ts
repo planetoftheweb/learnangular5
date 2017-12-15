b48d425373fa27f4c9157b73ee5a1ae6acb2bea0
Ray Villalobos <ray@planetoftheweb.com>
2017-12-15 11:31:56 -0800

    02_03e

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,8 +17,12 @@ export class AppComponent {
   query: string;
   artists: object;
 
+  showArtist(item) {
+    this.query = item.name;
+  }
+
   constructor() {
-    this.query = 'Barot';
+    this.query = '';
     this.artists = [
       {
         "name":"Barot Bellingham",
