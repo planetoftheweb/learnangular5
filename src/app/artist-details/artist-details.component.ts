663b14d45659901f3f83d145083a17a0375246b5
Ray Villalobos <ray@planetoftheweb.com>
2017-12-16 17:34:21 -0800

    finished project

--- src/app/artist-details/artist-details.component.ts
+++ src/app/artist-details/artist-details.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-artist-details',
-  templateUrl: './artist-details.component.html'
+  templateUrl: './artist-details.component.html',
+  inputs: ['artist']
 })
 export class ArtistDetailsComponent implements OnInit {
 
